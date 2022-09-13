e98a85e69d41543dce512f726dfebdbf533790d5
Miguel Pico <miguelanngel@hotmail.es>
2022-09-13 12:45:12 -0500

    adding local storage example into counter

--- lectures/lecture5 javascript/counter.js
+++ lectures/lecture5 javascript/counter.js
@@ -1,8 +1,12 @@
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
 function count() {
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
 }
 
 /* addEventListener takes 2 arguments, the event and the function 
@@ -10,9 +14,12 @@ which content will be executed after the event selected is completed */
 /* DOMContentLoaded waits until all the content on the page is loaded */
 document.addEventListener('DOMContentLoaded', function() {
     
+    // sets the h1 value with current local storage value
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+
     // document.querySelector('button').addEventListener('click', count);
     document.querySelector('button').onclick = count;
 
-    // run count function with an interval of 1000 milisecs or 1 sec
-    setInterval(count, 1000);
+    /* run count function with an interval of 1000 milisecs or 1 sec
+    setInterval(count, 1000);*/
 });
