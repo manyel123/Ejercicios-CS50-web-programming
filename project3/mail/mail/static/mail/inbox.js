07a64c3c2a99b0618e9a54dad7fcea4826ba281d
Miguel Angel Pico <miguelanngel@hotmail.es>
2022-12-10 20:36:37 -0500

    added function to display sent emails

--- project3/mail/mail/static/mail/inbox.js
+++ project3/mail/mail/static/mail/inbox.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -9,6 +9,8 @@ document.addEventListener('DOMContentLoaded', function() {
   // Submit the form to send a new email
   document.querySelector('#compose-form').onsubmit = send_email;
 
+  // 
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -18,10 +20,11 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-  
+
+  /*
   document.querySelector('#single-email-view').style.display = 'none';
   document.querySelectorAll("button").forEach(button => button.classList.remove("selected"));
-  document.querySelector(`#compose`).classList.add("selected");
+  document.querySelector(`#compose`).classList.add("selected");*/
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -53,39 +56,86 @@ function send_email() {
   })
     // Turns response into a JSON
     .then(response => response.json())
-      .then(result => {
-        if ("message" in result) {
-            // The email was sent successfully!
-            load_mailbox('sent');
-        }
+    .then(result => {
+      if ("message" in result) {
+        // The email was sent successfully!
+        load_mailbox('sent');
+      }
 
-        if ("error" in result) {
-            // If there was an error in sending the email
-            // Display the error next to the "To:"
-            document.querySelector('#to-text-error-message').innerHTML = result['error']
+      if ("error" in result) {
+        // If there was an error in sending the email
+        // Display the error next to the "To:"
+        document.querySelector('#to-text-error-message').innerHTML = result['error']
 
-        }
-        // console.log(result);
-        // console.log("message" in result);
-        // console.log("error" in result);
-      })
-      
-        // Catch the error 
-        .catch(error => {
-            // Print the error
-            console.log(error);
-        });
+      }
+      // console.log(result);
+      // console.log("message" in result);
+      // console.log("error" in result);
+    })
+
+    // Catch the error 
+    .catch(error => {
+      // Print the error
+      console.log(error);
+    });
   return false;
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Update mailboxes with the latest emails (inbox, sent, archive)
+  fetch(`/emails/${mailbox}`)
+
+    // Turns response into a JSON
+    .then(response => response.json())
+
+    .then(emails => {
+
+      // Defines elements to be displayed in preview, the name and the size for each element
+      const sections_to_display = [['sender', 5], ['subject', 3], ['timestamp', 4]];
+
+      // Defines email content to be displayed in preview
+      const email_content_to_display = {
+        'sender': 'Sender', 'subject': 'Subject', 'timestamp': 'Date and time', 'read': true
+      };
+      
+      emails = [email_content_to_display, ...emails];
+      
+      emails.forEach(email => {
+        const row_div_element = document.createElement('div');
+        row_div_element.classList.add("row", "email-line-box", email["read"] ? "read" : "unread");
+        
+        if (email === email_content_to_display) { row_div_element.id = 'titled-first-row'; }
+        sections_to_display.forEach(
+          section => {
+            const section_name = section[0];
+            const section_size = section[1];
+            const div_section = document.createElement('div');
+            div_section.classList.add(`col-${section_size}`, `${section_name}-section`);
+            div_section.innerHTML = `<p>${email[section_name]}</p>`;
+            row_div_element.append(div_section);
+
+          });/*
+        if (email !== artificial_first_email) {
+          row_div_element.addEventListener('click', () => load_email(email["id"], mailbox));
+        }*/
+
+        document.querySelector('#emails-view').append(row_div_element);
+      })
+
+    })
+    // Catch the error 
+    .catch(error => {
+      // Print the error
+      console.log(error);
+    });
 }
 
 
